39f095b83bec4d014227195703db842c05527bf3
Liquan Yang <yang.liquan87@gmail.com>
2020-05-30 20:10:51 +0800

    Edit test app settings

--- tests/settings.py
+++ tests/settings.py
@@ -1,7 +1,7 @@
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "drF-c0nf1D3n714!"
-
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 INSTALLED_APPS = [
@@ -29,5 +29,8 @@ REST_FRAMEWORK = {
 }
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:",},
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    },
 }
